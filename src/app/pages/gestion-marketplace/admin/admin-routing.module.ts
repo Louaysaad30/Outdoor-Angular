8ae1c40c5b30e85f53a2c4242ee0f5af663b7015
MajedSmichi <smichimajed@gmail.com>
2025-03-26 02:26:02 +0100

    admin routes

--- src/app/pages/gestion-marketplace/admin/admin-routing.module.ts
+++ src/app/pages/gestion-marketplace/admin/admin-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 
-const routes: Routes = [{path:'add-product',component:AddProductComponent}];
+const routes: Routes = [{path:'add-product', component:AddProductComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
