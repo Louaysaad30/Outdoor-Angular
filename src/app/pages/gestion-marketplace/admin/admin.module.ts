4c9799c764cb6af88f2d90786a1c09a679132e1a
MajedSmichi <smichimajed@gmail.com>
2025-03-26 02:05:19 +0100

    first commit

--- src/app/pages/gestion-marketplace/admin/admin.module.ts
+++ src/app/pages/gestion-marketplace/admin/admin.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { DropzoneModule } from 'ngx-dropzone-wrapper';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    CKEditorModule,
+    NgSelectModule,
+    DropzoneModule
+
   ]
 })
 export class AdminModule { }
