ef1548752039e667783efd2f871ab164d45169c3
Louaysaad30 <louay.saad@esprit.tn>
2025-04-26 18:00:25 +0100

    Add models and services for reviews and reservations; enhance camping center management

--- src/app/pages/gestion-camping/user/user-routing.module.ts
+++ src/app/pages/gestion-camping/user/user-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CampingfrontComponent} from "./campingfront/campingfront.component";
+import {DetailCentreComponent} from "./detail-centre/detail-centre.component";
+import {MesReservationComponent} from "./mes-reservation/mes-reservation.component";
 
 const routes: Routes = [
   {path:"camping",component:CampingfrontComponent},
+  { path: "camping/detail/:id", component:DetailCentreComponent  },
+  {path:"myReservation",component:MesReservationComponent}
+
+
 
 ];
 
